a787833d38c2a4c6f224c614c0dee78366257ab5
zevik7 <zevik2807@gmail.com>
2022-10-03 15:51:38 +0700

    build: setup sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,14 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
 import HomeBanner from '../components/HomeBanner'
+import { getClient } from '../sanity/sanity.server'
+import { Post } from '../typings'
+interface Props {
+  posts: [Post]
+}
 
-const Home: NextPage = () => {
+export default function Home({ posts }: Props) {
+  console.log(posts)
   return (
     <div className="home-page max-w-screen-2xl m-auto">
       <Head>
@@ -22,4 +28,24 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"] {
+      _id,
+      title,
+      author -> {
+      name,
+      image
+      },
+      description,
+      mainImage,
+      slug,
+    }`
+
+  const posts = await getClient().fetch(query)
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
